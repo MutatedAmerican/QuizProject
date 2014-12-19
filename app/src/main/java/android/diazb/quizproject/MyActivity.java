991c9c20812b0697d72d53323c4c6963dfa50b3a
Bryan Diaz <bryandiaz2010@yahoo.com>
2014-12-19 12:17:59 -0800

    Final Project Set (Hopefully will fix later)

--- app/src/main/java/android/diazb/quizproject/MyActivity.java
+++ app/src/main/java/android/diazb/quizproject/MyActivity.java
@@ -1,36 +1,46 @@
-package android.diazb.quizproject;
-
-import android.app.Activity;
+package android.diazb.quizproject;import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MyActivity extends Activity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
-    }
+
+        View answer = findViewById(R.id.toast_layout_root);
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.my, menu);
-        return true;
-    }
+        public void onClick(View answer) {
+            TextView text = (TextView) layout.findViewById(R.id.textView);
+            text.setText("Yes");
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+            Toast toast = new Toast(getApplicationContext());
+            toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+            toast.setDuration(Toast.LENGTH_LONG);
+            toast.setView(layout);
+            toast.show();
         }
-        return super.onOptionsItemSelected(item);
+
+        public void onClick(View answer) {
+            View layout = findViewById(R.id.toast_layout_root);
+
+            TextView text = (TextView) layout.findViewById(R.id.textView);
+            text.setText("No");
+
+            Toast toast = new Toast(getApplicationContext());
+            toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+            toast.setDuration(Toast.LENGTH_LONG);
+            toast.setView(layout);
+            toast.show();
+        }
     }
 }
